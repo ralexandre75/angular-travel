1ee995b1e5a5594d96a10f2bc8f5ca05a76a0370
rene.alexandre <rene.alexandre@grimoire-design.fr>
2018-07-03 10:30:09 +0200

    creation du module flight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { StayComponent } from './stay/stay.component';
+import { FlightComponent } from './flight/flight.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    StayComponent
+    StayComponent,
+    FlightComponent
   ],
   imports: [
     BrowserModule
