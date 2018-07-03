6785c481733b4e7ad6db50f8ef68a0bc1bc78ed4
rene.alexandre <rene.alexandre@grimoire-design.fr>
2018-07-03 10:26:17 +0200

    creation du module stay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StayComponent } from './stay/stay.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StayComponent
   ],
   imports: [
     BrowserModule
